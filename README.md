e46f22057cdd79504a8a8803c9df6ce56456f3e2
Dario Galvis <sdreyesg@gmail.com>
2022-07-09 18:27:25 -0400

    allocate and deallocate stack between tests

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # data_locality
-# data_locality
+
+This project sets a 4GB limit on the stack to test the implications of having data stored rowed vs columnar style and it's implications in performance
+
--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,7 @@ fn main() {
     .stack_size(MAX_MEMORY);
 
     let handler = builder.spawn(|| {
-
-        const CELLS_QTY:usize  = 10000000;
+        const CELLS_QTY:usize  = 150000000;
         let mut cells = [Cell{ value: 1.0, formula: 1, dependants: 2 }; CELLS_QTY ];
         
         println!("Sizeof Struct: {}", size_of_val(&cells[0]));
@@ -27,19 +26,28 @@ fn main() {
             cells[(n * 3) + 2].value = cells[(n * 3)].value + cells[(n * 3) + 1].value; 
         }
         println!("Struct version {}", now.elapsed().as_millis());
-    
-         
+    }).unwrap();
+
+    handler.join().unwrap();
+
+    let builder2 = thread::Builder::new()
+    .name("reductor".into())
+    .stack_size(MAX_MEMORY);
+    let handler2 = builder2.spawn(|| {
+        const CELLS_QTY:usize  = 150000000;
+        
         let mut values = [1.0; CELLS_QTY ];
         //let formulas = [1; CELLS_QTY ];
         //let dependants = [1; CELLS_QTY ];    
     
         let now2= Instant::now();
-        for n in 0..(CELLS_QTY - 1)/ 3 {
-            values[(n * 3) + 2] = values[(n * 3)] + values[(n * 3) + 1]; 
+        let chunk_size = (CELLS_QTY - 1)/ 3;
+        for n in 0..chunk_size - 1 {
+            values[(chunk_size * 2) + n] = values[n] + values[chunk_size + n]; 
         }
         
         println!("Columnar version {}", now2.elapsed().as_millis());
     }).unwrap();
 
-    handler.join().unwrap();
+    handler2.join().unwrap();
 }
